05e3c7719b3ad269287d03393a9e9166e8ec3a14
Volodymyr Kostenko <v.kostenko@svitla.com>
2016-01-25 11:56:05 +0200

    acc_js-#07: installing Gulp and all modules

--- gulpCompile.bat
+++ gulpCompile.bat
@@ -4,6 +4,8 @@ echo Style compilation started
 set startupPath= %~dp0
 echo %startupPath%
 CD %startupPath%
+npm install gulp -g
+npm install gulp
 call npm install -?save
 gulp watch
 echo Style compilation finished
--- gulpCompile.sh
+++ gulpCompile.sh
@@ -1,9 +1,11 @@
-@echo off
+echo off
 
 echo Style compilation started
 set startupPath= %~dp0
 echo %startupPath%
 cd %startupPath%
+npm install gulp -g
+npm install gulp
 npm install -?save
 gulp watch
 echo Style compilation finished
